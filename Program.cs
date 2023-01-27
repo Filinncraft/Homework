12726a51073251dfbe872072b84bcba6e35a2777
Mikhail Fedulov <fedulovmc@gmail.com>
2023-01-27 21:03:57 +0300

    Creation array

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string[] Array()
+{
+    Console.Write("Введите число элементов массива: ");
+    string [] arr = new string [Convert.ToInt32(Console.ReadLine())];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+void ShowArray (string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+ShowArray(Array());
+
