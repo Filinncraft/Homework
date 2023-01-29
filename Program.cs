258b4f6396b2a2ed259bf5bd111f33ab18a9301d
Mikhail Fedulov <fedulovmc@gmail.com>
2023-01-29 22:22:07 +0300

    Tiping readme

--- Program.cs
+++ Program.cs
@@ -15,22 +15,20 @@ void ShowArray (string[] arr)
         Console.Write($"{arr[i]} ");
     }
 }
-int Number(string[] arr)
+int Number(string[] arr, int limit)
 {
     int count = 0;
-    int limit = 4;
-    for (int i = 0; i < arr.Length; i++)
+        for (int i = 0; i < arr.Length; i++)
     {
         if (arr[i].Length < limit)
         count++;
     }
     return count;
 }
-string[] Final(string[] arr, int size)
+string[] Final(string[] arr, int size, int limit)
 {
     string [] fin = new string [size];
     int i = 0;
-    int limit = 4;
     for (int j = 0; j < arr.Length; j++)
     {
         if (arr[j].Length < limit)
@@ -44,8 +42,9 @@ string[] Final(string[] arr, int size)
 
 Console.Write("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
+int limit = 4;
 string[] first = Array(size);
 ShowArray(first);
 Console.WriteLine();
-int second = Number(first);
-ShowArray(Final(first, second));
+int second = Number(first, limit);
+ShowArray(Final(first, second, limit));
